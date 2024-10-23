dd2491adaa705cc8136387a6aab9d4f2e9edffad
masoud zarepour <151038575+mdzarepour@users.noreply.github.com>
2024-10-23 15:36:59 +0330

    cleaned

--- lib/components/api_constants.dart
+++ lib/components/api_constants.dart
@@ -16,10 +16,7 @@ class ApiConstants {
       "${baseUrl}podcast/get.php?command=published_by_me&user_id=1";
   static const String getPodcastFiles =
       "${baseUrl}podcast/get.php?command=get_files&podcats_id=1";
-  static const String getHomeItems = "${baseUrl}home/?command=index";
-  static const String hostDonwload = "https://techblog.sasansafari.com";
-  static const String baseUrl =
-      "https://techblog.sasansafari.com/Techblog/api/";
+  static const String getHomeItems = "${baseUrl}home/?command=index" ;
   static const String getInfo = "${baseUrl}user/?command=info&user_id=2";
   static const String getAppCategories = "${baseUrl}article/get.php?command=categories";
   static const String getTags = "${baseUrl}article/get.php?command=tags";
